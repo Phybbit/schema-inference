c88cc7f2edfe628a4c2635dff7d7980bdd7f0c12
Eurico Doirado <eurico@phybbit.com>
2016-09-03 18:04:35 +0900

    Moved the tests to its own file and kept the inference_spec to test the interface.

---
+++ spec/schema/inference/schema_inferrer_spec.rb
@@ -0,0 +1,100 @@
+require 'spec_helper'
+
+describe Schema::Inference::SchemaInferrer do
+
+  describe '#schema' do
+    let (:inferrer) { Schema::Inference::SchemaInferrer.new }
+
+    it 'recognizes numerics' do
+      dataset = [
+        { 'numeric' => 1 },
+        { 'numeric' => 1.5 },
+        { 'numeric' => '1' },
+        { 'numeric' => '1.5' },
+      ]
+      expect(inferrer.infer_schema(dataset: dataset)['numeric'][:type]).to eq Numeric
+    end
+
+    it 'recognizes integers' do
+      dataset = [
+        { 'integer' => 1 },
+        { 'integer' => '1' },
+      ]
+      expect(inferrer.infer_schema(dataset: dataset)['integer'][:type]).to eq Integer
+    end
+
+    it 'recognizes booleans' do
+      dataset = [
+        { 'bool' => true },
+        { 'bool' => false },
+        { 'bool' => 'true' },
+        { 'bool' => 'false' },
+        { 'bool' => 'TRUE' },
+        { 'bool' => 'FALSE' },
+      ]
+      expect(inferrer.infer_schema(dataset: dataset)['bool'][:type]).to eq Boolean
+    end
+
+    it 'recognizes strings' do
+      dataset = [
+        { 'string' => 'a string' },
+        { 'string' => 'and another string' },
+      ]
+      expect(inferrer.infer_schema(dataset: dataset)['string'][:type]).to eq String
+    end
+
+    it 'recognizes fields with multiple types as Object' do
+      dataset = [
+        { 'multiple' => 1 },
+        { 'multiple' => 'was an int but became a string' },
+      ]
+      expect(inferrer.infer_schema(dataset: dataset)['multiple'][:type]).to eq Object
+    end
+
+    it 'recognizes (terminal) arrays' do
+      dataset = [
+        { 'array' => [0, 1, nil, 2] },
+        { 'array' => [4] },
+      ]
+      expect(inferrer.infer_schema(dataset: dataset)['array'][:type]).to eq Array
+      expect(inferrer.infer_schema(dataset: dataset)['array'][:min_size]).to eq 1
+      expect(inferrer.infer_schema(dataset: dataset)['array'][:max_size]).to eq 4
+    end
+
+    it 'recognizes (terminal) arrays content (extended)' do
+      dataset = [
+        { 'array' => [1] },
+      ]
+      expect(inferrer.infer_schema(dataset: dataset, extended: true)['array.0'][:type]).to eq Integer
+    end
+
+    it 'recognizes fields in hashes' do
+      dataset = [
+        { 'in' => { 'hash' => 'some string' } }
+      ]
+      expect(inferrer.infer_schema(dataset: dataset)['in.hash'][:type]).to eq String
+    end
+
+    it 'recognizes fields in arrays' do
+      dataset = [
+        { 'array' => [{ 'deep' => 'structure' }] }
+      ]
+      expect(inferrer.infer_schema(dataset: dataset)['array.0.deep'][:type]).to eq String
+    end
+
+    it 'recognizes nils' do
+      dataset = [
+        { 'with_nulls' => nil },
+      ]
+      expect(inferrer.infer_schema(dataset: dataset)['with_nulls'][:type]).to eq NilClass
+    end
+
+    it 'removes null keys that have full keys with other objects' do
+      dataset = [
+        { 'with_nils' => nil },
+        { 'with_nils' => [4] },
+      ]
+      expect(inferrer.infer_schema(dataset: dataset)['with_nils'][:type]).to eq Array
+    end
+  end
+end
--- spec/schema/inference_spec.rb
+++ spec/schema/inference_spec.rb
@@ -8,96 +8,13 @@ describe Schema::Inference do
   describe '#schema' do
     let (:ns) { Schema::Inference }
 
-    it 'recognizes numerics' do
+    it 'support a default interface' do
       dataset = [
         { 'numeric' => 1 },
-        { 'numeric' => 1.5 },
-        { 'numeric' => '1' },
-        { 'numeric' => '1.5' },
+        { 'hash' => { 'value' => 'string' }}
       ]
-      expect(ns.schema(dataset: dataset)['numeric'][:type]).to eq Numeric
-    end
-
-    it 'recognizes integers' do
-      dataset = [
-        { 'integer' => 1 },
-        { 'integer' => '1' },
-      ]
-      expect(ns.schema(dataset: dataset)['integer'][:type]).to eq Integer
-    end
-
-    it 'recognizes booleans' do
-      dataset = [
-        { 'bool' => true },
-        { 'bool' => false },
-        { 'bool' => 'true' },
-        { 'bool' => 'false' },
-        { 'bool' => 'TRUE' },
-        { 'bool' => 'FALSE' },
-      ]
-      expect(ns.schema(dataset: dataset)['bool'][:type]).to eq Boolean
-    end
-
-    it 'recognizes strings' do
-      dataset = [
-        { 'string' => 'a string' },
-        { 'string' => 'and another string' },
-      ]
-      expect(ns.schema(dataset: dataset)['string'][:type]).to eq String
-    end
-
-    it 'recognizes fields with multiple types as Object' do
-      dataset = [
-        { 'multiple' => 1 },
-        { 'multiple' => 'was an int but became a string' },
-      ]
-      expect(ns.schema(dataset: dataset)['multiple'][:type]).to eq Object
-    end
-
-    it 'recognizes (terminal) arrays' do
-      dataset = [
-        { 'array' => [0, 1, nil, 2] },
-        { 'array' => [4] },
-      ]
-      expect(ns.schema(dataset: dataset)['array'][:type]).to eq Array
-      expect(ns.schema(dataset: dataset)['array'][:min_size]).to eq 1
-      expect(ns.schema(dataset: dataset)['array'][:max_size]).to eq 4
-    end
-
-    it 'recognizes (terminal) arrays content (extended)' do
-      dataset = [
-        { 'array' => [1] },
-      ]
-      expect(ns.schema(dataset: dataset, extended: true)['array.0'][:type]).to eq Integer
-    end
-
-    it 'recognizes fields in hashes' do
-      dataset = [
-        { 'in' => { 'hash' => 'some string' } }
-      ]
-      expect(ns.schema(dataset: dataset)['in.hash'][:type]).to eq String
-    end
-
-    it 'recognizes fields in arrays' do
-      dataset = [
-        { 'array' => [{ 'deep' => 'structure' }] }
-      ]
-      expect(ns.schema(dataset: dataset)['array.0.deep'][:type]).to eq String
-    end
-
-    it 'recognizes nils' do
-      dataset = [
-        { 'with_nulls' => nil },
-      ]
-      expect(ns.schema(dataset: dataset)['with_nulls'][:type]).to eq NilClass
-    end
-
-    it 'removes null keys that have full keys with other objects' do
-      dataset = [
-        { 'with_nils' => nil },
-        { 'with_nils' => [4] },
-      ]
-      expect(ns.schema(dataset: dataset)['with_nils'][:type]).to eq Array
+      expect(ns.schema(dataset: dataset)['numeric'][:type]).to eq Integer
+      expect(ns.schema(dataset: dataset)['hash.value'][:type]).to eq String
     end
   end
 end
