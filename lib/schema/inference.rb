5b8a94548c76a911bb8a0e46fcc960a5753ead47
Eurico Doirado <eurico@phybbit.com>
2016-09-03 17:29:55 +0900

    Added main dependencies.

--- lib/schema/inference.rb
+++ lib/schema/inference.rb
@@ -1,7 +1,21 @@
+require 'active_support'
+require 'active_support/core_ext/object/blank'
+require 'active_support/core_ext/hash/reverse_merge'
+
+require 'parallel'
+require 'timeliness'
+
+require 'extensions/boolean'
 require 'schema/inference/version'
+require 'schema/inference/schema_inferrer'
+
 
 module Schema
   module Inference
-    # Your code goes here...
+
+    def Inference.schema(*args)
+      SchemaInferrer.infer_schema(*args)
+    end
+
   end
 end
--- schema-inference.gemspec
+++ schema-inference.gemspec
@@ -21,4 +21,9 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'bundler', '~> 1.12'
   spec.add_development_dependency 'rake', '~> 10.0'
   spec.add_development_dependency 'rspec', '~> 3.0'
+  spec.add_development_dependency 'pry-byebug'
+
+  spec.add_dependency 'activesupport', '>= 4.0.0'
+  spec.add_dependency 'parallel', '~>1.8'
+  spec.add_dependency 'timeliness', '~>0.3'
 end
